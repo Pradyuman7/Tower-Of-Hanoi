272c253184b8805fb7b988c10ba43ce11c2752dd
Pradyuman Dixit <41565823+Pradyuman7@users.noreply.github.com>
2018-09-21 16:43:44 +0200

    Update main.cpp

--- Tower Of Hanoi/main.cpp
+++ Tower Of Hanoi/main.cpp
@@ -8,8 +8,25 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void TOH(int n,char x,char y,char z){
+    if(n==1)
+        std::cout<<"Move top disk from "<<x<<" to "<<z<<std::endl;
+    else{
+        TOH(n-1,x,z,y);
+        TOH(1,x,y,z);
+        TOH(n-1,y,x,z);
+    }
+    
+}
+
+int main() {
+    using namespace std;
+    int num;
+    char x,y,z;
+    cout << "Hello, How many disks you want to make your tower with?\n";
+    cin>>num;
+    cout<<"Name your towers respetively\n";
+    cin>>x>>y>>z;
+    TOH(num,x,y,z);
     return 0;
 }
